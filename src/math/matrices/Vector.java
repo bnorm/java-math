a63103e9c0a6992ea441613e7d1ecf1a8b4f59c3
Brian Norman <brian.e.norman@gmail.com>
2013-07-21 13:23:33 -0400

    Overriding IVector return self methods
    Sorting methods better and overriding all of the methods from IVector
    that return IVector to return the more specific type, Vector.

--- src/math/matrices/Vector.java
+++ src/math/matrices/Vector.java
@@ -1,18 +1,13 @@
 package math.matrices;
 
-
 /**
- * The object representation of a mathematical vector. This class provides an implementation for basic use. This is not
- * a safe vector. It does not perform any dimension checks before performing any actions.
+ * The object representation of a mathematical vector.  This class provides an implementation for basic use.  This is
+ * not a safe vector. It does not perform any dimension checks before performing any actions.
  *
  * @author Brian Norman
  * @version 0.1 beta
  */
 public class Vector extends Matrix implements IVector {
-
-   // ************************ //
-   // ***** CONSTRUCTORS ***** //
-   // ************************ //
 
    /**
     * Creates a new vector with no values and of size zero.
@@ -31,7 +26,7 @@ public class Vector extends Matrix implements IVector {
    }
 
    /**
-    * Creates a new row vector with the specified array as a base. if the resulting vector is modified the original
+    * Creates a new row vector with the specified array as a base.  If the resulting vector is modified the original
     * vector will change as well.
     *
     * @param v the base vector of the matrix.
@@ -45,20 +40,117 @@ public class Vector extends Matrix implements IVector {
     *
     * @param v the vector to copy.
     */
-   protected Vector(Vector v) {
+   protected Vector(IVector v) {
       super(v);
    }
 
-   // *************************** //
-   // ***** UTILITY METHODS ***** //
-   // *************************** //
+   @Override
+   public Vector set(double[] v) {
+      return (Vector) IVector.super.set(v);
+   }
 
-   /**
-    * {@inheritDoc}
-    */
+   @Override
+   public Vector set(double[][] a) {
+      return (Vector) super.set(a);
+   }
+
+   @Override
+   public Vector set(int i, double n) {
+      return (Vector) IVector.super.set(i, n);
+   }
+
+   @Override
+   public Vector set(int r, int c, double n) {
+      return (Vector) super.set(r, c, n);
+   }
+
+   @Override
+   public Vector setRow(int r, double[] v) {
+      return (Vector) super.setRow(r, v);
+   }
+
+   @Override
+   public Vector setColumn(int c, double[] v) {
+      return (Vector) super.setColumn(c, v);
+   }
+
+   @Override
+   public Vector set(int i, double[] v) {
+      return (Vector) IVector.super.set(i, v);
+   }
+
+   @Override
+   public Vector set(int r, int c, double[][] a) {
+      return (Vector) super.set(r, c, a);
+   }
+
+   @Override
+   public Vector transpose() {
+      return (Vector) super.transpose();
+   }
+
+   @Override
+   public Vector scale(double n) {
+      return (Vector) super.scale(n);
+   }
+
+   @Override
+   public Vector inverse() {
+      return (Vector) super.inverse();
+   }
+
+   @Override
+   public Vector add(IVector v) {
+      return (Vector) IVector.super.add(v);
+   }
+
+   @Override
+   public Vector add(IMatrix a) {
+      return (Vector) super.add(a);
+   }
+
+   @Override
+   public Vector subtract(IVector v) {
+      return (Vector) IVector.super.subtract(v);
+   }
+
+   @Override
+   public Vector subtract(IMatrix a) {
+      return (Vector) super.subtract(a);
+   }
+
+   @Override
+   public Matrix outer(IVector v) {
+      return (Matrix) IVector.super.outer(v);
+   }
+
+   @Override
+   public Vector multiply(IMatrix a) {
+      return (Vector) super.multiply(a);
+   }
+
+   @Override
+   public Vector dotMultiply(IMatrix a) {
+      return (Vector) super.dotMultiply(a);
+   }
+
+   @Override
+   public Vector dotMultiply(IVector v) {
+      return (Vector) IVector.super.dotMultiply(v);
+   }
+
+   @Override
+   public Vector dotDivide(IMatrix a) {
+      return (Vector) super.dotDivide(a);
+   }
+
+   @Override
+   public Vector dotDivide(IVector v) {
+      return (Vector) IVector.super.dotDivide(v);
+   }
+
    @Override
    public Vector copy() {
       return new Vector(this);
    }
-
 }
